0426fe23c762fd766136c2de0ffa0d82e6b784ed
Pedro Nieto <pedro.nieto@onlyfy.com>
2024-01-17 17:25:18 +0100

    update data

--- client/main.py
+++ client/main.py
@@ -1,14 +1,25 @@
 import json
 import os
 import time
+import pandas as pd
+import random
 
 user_id=os.getenv('USER_ID')
 topic_id=os.getenv('TOPIC_ID')
 time_lapse=int(os.getenv('TIME_ID'))
 
+counter=0
+lines=[]
+
 def generatedata():
+    global counter
+    counter=counter+1
+    if counter>=len(lines):
+        counter=0
     data={}
     data["userid"]=user_id
+    data["timestamp"]=int(time.time())
+    data["heart_rate"]=lines[counter]*100+random.randint(0,100)
     return json.dumps(data)
 
 def senddata():
@@ -18,7 +29,15 @@ def senddata():
     print(generatedata())
 
 
+with open('data/data.csv', 'r') as file:
+    lines = file.readlines()
+
+lines = [float(line.strip()) for line in lines]
+    
+print(lines)
+
 
 while True:
     senddata()
     time.sleep(time_lapse)
+
